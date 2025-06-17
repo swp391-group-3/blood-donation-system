4e0fb6d3014872e9cad5519b2c6f64414cda59fb
Lunarcd <nhatanhth0212@gmail.com>
2025-06-17 15:51:05 +0700

    feat(request-page): new dto

--- frontend/src/lib/api/dto/blood-request.ts
+++ frontend/src/lib/api/dto/blood-request.ts
@@ -8,9 +8,11 @@ export type Priority = (typeof priorities)[number];
 export const displayPriority = (priority: Priority) => capitalCase(priority);
 
 export interface BloodRequest {
-    blood_group: BloodGroup;
+    blood_groups: BloodGroup[];
     priority: Priority;
     title: string;
+    current_people: number;
+    max_people: number;
     start_time: Date;
     end_time: Date;
 }
