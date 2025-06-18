8a081bd42794663e649f4b675773dcdfce9b563e
ChiBao <baotochi87@gmail.com>
2025-06-18 18:21:34 +0700

    feat: uncomment Header in main layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
                 <QueryProvider>
-                    {/* <Header /> */}
+                    <Header />
                     {children}
                     <Toaster richColors />
                 </QueryProvider>
