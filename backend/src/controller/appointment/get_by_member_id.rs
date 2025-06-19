941d1674f08446fe39c30389759aade47e47b6b3
akagiyuu <khanghuynh0245@gmail.com>
2025-06-19 14:07:16 +0700

    feat(appointment): add title

--- backend/src/controller/appointment/get_by_member_id.rs
+++ backend/src/controller/appointment/get_by_member_id.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use axum::{Json, extract::State};
 use chrono::{DateTime, Utc};
 use ctypes::AppointmentStatus;
-use database::queries::{self};
+use database::queries::{self, appointment::GetByMemberIdBorrowed};
 use model_mapper::Mapper;
 use serde::Serialize;
 use utoipa::ToSchema;
@@ -12,11 +12,12 @@ use uuid::Uuid;
 use crate::{error::Result, state::ApiState, util::jwt::Claims};
 
 #[derive(Serialize, ToSchema, Mapper)]
-#[mapper(from, ty = queries::appointment::GetByMemberId)]
+#[mapper(from, ty = GetByMemberIdBorrowed::<'_>)]
 pub struct Appointment {
     pub id: Uuid,
     pub request_id: Uuid,
     pub member_id: Uuid,
+    pub title: String,
     pub status: AppointmentStatus,
     pub start_time: DateTime<Utc>,
     pub end_time: DateTime<Utc>,
@@ -40,7 +41,7 @@ pub async fn get_by_member_id(
 
     let appointments = queries::appointment::get_by_member_id()
         .bind(&database, &claims.sub)
-        .map(Appointment::from)
+        .map(|raw| Appointment::from(raw))
         .all()
         .await?;
 
