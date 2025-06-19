47808846bf919c88f84daadd53999b9ead793c07
namdang-fdp <namdangcoder@gmail.com>
2025-06-19 09:50:28 +0700

    feat: add useEffect to add dark style

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,14 @@
 'use client';
+import { useEffect } from "react";
+import { PremiumTestimonials } from "@/components/ui/premium-testimonials";
 
 export default function MemberHomePage() {
-    return <p>Home</p>;
+  useEffect(() => {
+    document.body.classList.add('dark');
+    return () => {
+      document.body.classList.remove('dark');
+    };
+  }, []);
+
+  return <PremiumTestimonials />;
 }
