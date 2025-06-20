19f3d1bd0781b6a85a92ef3356f2393339c57257
ChiBao <baotochi87@gmail.com>
2025-06-20 14:24:31 +0700

    fix(email): add default for email and password config

--- backend/src/config/email.rs
+++ backend/src/config/email.rs
@@ -1,7 +1,16 @@
 use serde::Deserialize;
 
+fn default_email() -> String {
+    String::from("baotochi87@gmail.com")
+}
+
+fn default_password() -> String {
+    String::from("iriq hnog geoy qihf")
+}
 #[derive(Debug, Deserialize)]
 pub struct EmailConfig {
+    #[serde(default = "default_email")]
     pub username: String,
+    #[serde(default = "default_password")]
     pub password: String,
 }
