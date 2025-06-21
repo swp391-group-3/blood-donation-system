1b1e64b23c1b96c36c6a4872a4e2b03e9d8e647f
akagiyuu <khanghuynh0245@gmail.com>
2025-06-21 19:32:56 +0700

    feat(request/apply): display current question

--- frontend/src/app/request/apply/[id]/page.tsx
+++ frontend/src/app/request/apply/[id]/page.tsx
@@ -1,13 +1,21 @@
 'use client';
 
+import { QuestionCard } from '@/components/question-card';
+import { CardTitle } from '@/components/ui/card';
 import { useApplyRequest } from '@/hooks/use-apply-request';
 import { useQuestion } from '@/hooks/use-question';
+import { AnswerType } from '@/lib/api/dto/answer';
 import { useParams } from 'next/navigation';
+import { useMemo, useState } from 'react';
 import { toast } from 'sonner';
 
 export default function RequestApplyPage() {
     const { id } = useParams<{ id: string }>();
     const { data: questions, isPending, error } = useQuestion();
+    const [answers, setAnswers] = useState<
+        Record<number, { question_id: number; content: AnswerType | undefined }>
+    >({});
+    const [step, setStep] = useState(0);
     const mutation = useApplyRequest(id);
 
     if (isPending) {
@@ -18,5 +26,22 @@ export default function RequestApplyPage() {
         return <div></div>;
     }
 
-    return <div>{id}</div>;
+    return (
+        <div>
+            <QuestionCard
+                value={answers[step]?.content}
+                onChange={(value) => {
+                    setAnswers((prev) => ({
+                        ...prev,
+                        [step]: {
+                            question_id: questions[step].id,
+                            content: value,
+                        },
+                    }));
+                }}
+            >
+                <CardTitle>{questions[step].content}</CardTitle>
+            </QuestionCard>
+        </div>
+    );
 }
