5795986406de8a1d863ef32ccb658be4890078fa
namdang-fdp <namdangcoder@gmail.com>
2025-06-24 15:01:02 +0700

    feat: add hooks to fetch data from api

---
+++ frontend/src/hooks/use-blood-storage-list.ts
@@ -0,0 +1,15 @@
+'use client'
+
+import { deserialize, fetchWrapper } from "@/lib/api"
+import { BloodBag } from "@/lib/api/dto/blood-bag"
+import { useQuery } from "@tanstack/react-query"
+
+export const useBloodStorageList = () => {
+    return useQuery({
+        queryFn: async () => {
+            const response = await fetchWrapper('/blood-bag');
+            return await deserialize<BloodBag[]>(response);
+        },
+        queryKey: ['blood-bag']
+    })
+}
