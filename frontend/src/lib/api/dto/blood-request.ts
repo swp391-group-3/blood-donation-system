6e83213a3bd347f4313d48e7ad295506ca37a63c
akagiyuu <khanghuynh0245@gmail.com>
2025-06-24 12:04:28 +0700

    fix(dto): add blood group to blood request

--- frontend/src/lib/api/dto/blood-request.ts
+++ frontend/src/lib/api/dto/blood-request.ts
@@ -1,3 +1,5 @@
+import { BloodGroup } from "./blood-group";
+
 export const priorities = ['low', 'medium', 'high'] as const;
 
 export type Priority = (typeof priorities)[number];
@@ -6,6 +8,7 @@ export interface BloodRequest {
     id: string;
     priority: Priority;
     title: string;
+    blood_groups: BloodGroup[];
     current_people: number;
     max_people: number;
     start_time: Date;
