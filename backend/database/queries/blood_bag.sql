2980f72ac951d59394e705b016b871f26621a125
Lunarcd <nhatanhth0212@gmail.com>
2025-06-25 16:03:28 +0700

    chore: adding blood group field to get blood bag query

--- backend/database/queries/blood_bag.sql
+++ backend/database/queries/blood_bag.sql
@@ -1,13 +1,51 @@
 --: BloodBag()
 
 --! get : BloodBag
-SELECT *
+SELECT 
+    id,
+    donation_id,
+    (
+        SELECT blood_group
+        FROM accounts
+        WHERE id = (
+            SELECT member_id
+            FROM appointments
+            WHERE id = (
+                SELECT appointment_id
+                FROM donations
+                WHERE id = blood_bags.donation_id
+            )
+        )
+    ) AS blood_group,
+    component,
+	is_used,
+	amount,
+	expired_time
 FROM blood_bags
 WHERE id = :id;
 
 --! get_all : BloodBag
-SELECT *
-FROM blood_bags;
+SELECT 
+    id,
+    donation_id,
+    (
+        SELECT blood_group
+        FROM accounts
+        WHERE id = (
+            SELECT member_id
+            FROM appointments
+            WHERE id = (
+                SELECT appointment_id
+                FROM donations
+                WHERE id = blood_bags.donation_id
+            )
+        )
+    ) AS blood_group,
+    component,
+	is_used,
+	amount,
+	expired_time
+FROM blood_bags
 
 --! create
 INSERT INTO blood_bags (
