e846e71318f473e3e4a926a9eff4f11be51cf5ec
Lunarcd <nhatanhth0212@gmail.com>
2025-06-25 22:04:41 +0700

    chore: small refactor

--- backend/src/controller/auth/logout.rs
+++ backend/src/controller/auth/logout.rs
@@ -6,9 +6,9 @@ use crate::{config::CONFIG, error::Result};
 #[utoipa::path(post, tag = "Auth", path = "/auth/logout")]
 pub async fn logout(jar: CookieJar) -> Result<CookieJar> {
     let mut cookie = Cookie::new(CONFIG.jwt.token_key.clone(), "");
-    cookie.make_removal();
     cookie.set_same_site(SameSite::Lax);
     cookie.set_path("/");
+    cookie.make_removal();
 
     Ok(jar.remove(cookie))
 }
