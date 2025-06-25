84e0b98bbaf1b28c36099ba638dc6b2547423f71
namdang-fdp <namdangcoder@gmail.com>
2025-06-25 20:08:31 +0700

    feat: more strict enhance

--- frontend/src/hooks/use-delete-bloodbag.ts
+++ frontend/src/hooks/use-delete-bloodbag.ts
@@ -1,24 +1,27 @@
-import { fetchWrapper } from "@/lib/api";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { toast } from "sonner";
+import { fetchWrapper } from '@/lib/api';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'sonner';
+import { BloodBag } from '@/lib/api/dto/blood-bag';
 
 export const useDeleteBloodBag = () => {
-  const queryClient = useQueryClient();
+    const queryClient = useQueryClient();
 
-  return useMutation({
-    mutationFn: async (id: string) => {
-      await fetchWrapper(`/blood-bag/${id}`, {
-        method: "DELETE",
-      });
-    },
-    onSuccess: (_, id) => {
-      queryClient.setQueryData(["blood-bag"], (prev: any) =>
-        Array.isArray(prev) ? prev.filter((bag) => bag.id !== id) : prev
-      );
-      toast.success("Blood bag deleted successfully");
-    },
-    onError: () => {
-      toast.error("Failed to delete blood bag");
-    },
-  });
+    return useMutation({
+        mutationFn: async (id: string) => {
+            await fetchWrapper(`/blood-bag/${id}`, {
+                method: 'DELETE',
+            });
+        },
+        onSuccess: (_, id) => {
+            queryClient.setQueryData<BloodBag[]>(['blood-bag'], (prev) =>
+                Array.isArray(prev)
+                    ? prev.filter((bag) => bag.id !== id)
+                    : prev,
+            );
+            toast.success('Blood bag deleted successfully');
+        },
+        onError: () => {
+            toast.error('Failed to delete blood bag');
+        },
+    });
 };
