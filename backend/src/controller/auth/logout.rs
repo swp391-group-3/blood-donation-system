8cf9e8e93715a7c37328a3f94d07b930da55f11a
Lunarcd <nhatanhth0212@gmail.com>
2025-06-25 21:43:20 +0700

    feat(auth): add logout API

---
+++ backend/src/controller/auth/logout.rs
@@ -0,0 +1,13 @@
+use axum_extra::extract::{CookieJar, cookie::Cookie};
+use tower_sessions::cookie::SameSite;
+
+use crate::{config::CONFIG, error::Result};
+
+#[utoipa::path(post, tag = "Auth", path = "/auth/logout")]
+pub async fn logout(jar: CookieJar) -> Result<CookieJar> {
+    let mut cookie = Cookie::new(CONFIG.jwt.token_key.clone(), "");
+    cookie.set_same_site(SameSite::Lax);
+    cookie.set_path("/");
+
+    Ok(jar.remove(cookie))
+}
