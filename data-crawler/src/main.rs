3c7cbbcebefba9c4adade47b6ebd676216c5405b
Lunarcd <nhatanhth0212@gmail.com>
2025-07-01 01:55:52 +0700

    chore: remove unnecessary code

--- data-crawler/src/main.rs
+++ data-crawler/src/main.rs
@@ -1,10 +1,9 @@
 use chromiumoxide::{
     browser::{Browser, BrowserConfig},
-    cdp::browser_protocol::{emulation::SetDeviceMetricsOverrideParams, page::PrintToPdfParams},
+    cdp::browser_protocol::page::PrintToPdfParams,
 };
 use futures::StreamExt;
-use std::{fs::create_dir_all, path::PathBuf, time::Duration};
-use tokio::time::sleep;
+use std::{fs::create_dir_all, path::PathBuf};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -13,10 +12,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = BrowserConfig::builder()
         .chrome_executable(chrome_path)
         .with_head()
-        .args(vec![
-            "--start-maximized".to_string(),
-            "--force-device-scale-factor=1".to_string(),
-        ])
         .build()?;
 
     let (mut browser, mut handler) = Browser::launch(config).await?;
@@ -24,18 +19,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let handle = tokio::spawn(async move { while let Some(_) = handler.next().await {} });
 
     let page = browser.new_page("https://www.redcrossblood.org/").await?;
-
-    page.execute(
-        SetDeviceMetricsOverrideParams::builder()
-            .width(1920)
-            .height(1080)
-            .device_scale_factor(1)
-            .mobile(false)
-            .build()
-            .map_err(|e| format!("Failed to build device metrics params: {}", e))?,
-    )
-    .await?;
-
     page.wait_for_navigation().await?;
 
     let all_links = page
@@ -51,41 +34,30 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 && href != "/donate-blood/manage-my-donations/rapidpass.html"
             {
                 let url = format!("https://www.redcrossblood.org{}", href);
-                
-                let subpage = browser.new_page(&url).await?;
-                subpage
-                    .execute(
-                        SetDeviceMetricsOverrideParams::builder()
-                            .width(1920)
-                            .height(1080)
-                            .device_scale_factor(1)
-                            .mobile(false)
-                            .build()
-                            .map_err(|e| format!("Failed to build device metrics params: {}", e))?,
-                    )
-                    .await?;
-                
-                subpage.wait_for_navigation().await?;
-                
-                let pdf_bytes = subpage
-                    .pdf(PrintToPdfParams::default())
-                    .await?;
 
-                let name = href
-                    .trim_start_matches('/')
+                let subpage = browser.new_page(&url).await?;
+                subpage.wait_for_navigation().await?;
+
+                let pdf_bytes = subpage.pdf(PrintToPdfParams::default()).await?;
+
+                let formatted_name = href
+                    .trim_start_matches("/donate-blood/blood-donation-process/")
+                    .trim_start_matches("/donate-blood/how-to-donate/")
+                    .trim_start_matches("/donate-blood/")
                     .trim_end_matches(".html")
                     .replace('/', "_")
                     .replace('-', "_");
 
-                let filename = format!("{}.pdf", name);
+                let filename = format!("{}.pdf", formatted_name);
                 let path = output_dir.join(filename);
                 tokio::fs::write(path, pdf_bytes).await?;
+
+                subpage.close().await?;
             }
         }
     }
 
-    sleep(Duration::from_secs(30)).await;
-
+    page.close().await?;
     browser.close().await?;
     handle.await?;
 
