cce3e3b7c01c5ddc6ff74fb0c9d8c357afe5eec6
Lunarcd <nhatanhth0212@gmail.com>
2025-06-30 22:41:04 +0700

    chore: open site

---
+++ data-crawler/src/main.rs
@@ -0,0 +1,43 @@
+use chromiumoxide::{browser::{Browser, BrowserConfig}, cdp::browser_protocol::emulation::SetDeviceMetricsOverrideParams};
+use futures::StreamExt;
+use std::time::Duration;
+use tokio::time::sleep;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let chrome_path = r#"C:\Program Files\Google\Chrome\Application\chrome.exe"#;
+
+    let config = BrowserConfig::builder()
+        .chrome_executable(chrome_path)
+        .with_head()
+        .args(vec![
+            "--start-maximized".to_string(),
+            "--force-device-scale-factor=1".to_string(),
+        ])
+        .build()?;
+
+    let (mut browser, mut handler) = Browser::launch(config).await?;
+
+    let handle = tokio::spawn(async move { while let Some(_) = handler.next().await {} });
+
+    let page = browser.new_page("https://www.redcrossblood.org/").await?;
+
+    page.execute(
+        SetDeviceMetricsOverrideParams::builder()
+            .width(1920)
+            .height(1080)
+            .device_scale_factor(1)
+            .mobile(false)
+            .build()
+            .map_err(|e| format!("Failed to build device metrics params: {}", e))?
+    ).await?;
+
+    page.wait_for_navigation().await?;
+    
+    sleep(Duration::from_secs(30)).await;
+
+    browser.close().await?;
+    handle.await?;
+
+    Ok(())
+}
