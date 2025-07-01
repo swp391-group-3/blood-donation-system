3e717494143f9bd07293903e6c5b3f355ca9ee43
Lunarcd <nhatanhth0212@gmail.com>
2025-07-01 19:02:29 +0700

    chore: small fix

--- data-crawler/src/main.rs
+++ data-crawler/src/main.rs
@@ -5,13 +5,7 @@ use std::{fs::create_dir_all, path::PathBuf};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let chrome_path = r#"C:\Program Files\Google\Chrome\Application\chrome.exe"#;
-
-    let config = BrowserConfig::builder()
-        .chrome_executable(chrome_path)
-        .with_head()
-        .build()?;
-
+    let config = BrowserConfig::builder().with_head().build()?;
     let (mut browser, mut handler) = Browser::launch(config).await?;
 
     let handle = tokio::spawn(async move { while let Some(_) = handler.next().await {} });
@@ -27,37 +21,38 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     create_dir_all(&output_dir)?;
 
     for link in all_links {
-        if let Some(href) = link.attribute("href").await? {
-            if href.starts_with("/donate-blood")
-                && href != "/donate-blood/manage-my-donations/rapidpass.html"
-            {
-                let url = format!("https://www.redcrossblood.org{}", href);
+        let Some(href) = link.attribute("href").await? else {
+            continue;
+        };
+        if href.starts_with("/donate-blood")
+            && href != "/donate-blood/manage-my-donations/rapidpass.html"
+        {
+            let url = format!("https://www.redcrossblood.org{}", href);
 
-                let subpage = browser.new_page(&url).await?;
-                subpage.wait_for_navigation().await?;
+            let subpage = browser.new_page(&url).await?;
+            subpage.wait_for_navigation().await?;
 
-                let html = subpage.content().await?;
-                let md = HtmlToMarkdown::builder()
-                    .skip_tags(vec!["script", "style", "header", "footer"])
-                    .add_handler(vec!["a"], |e: Element| Some(e.content.to_string()))
-                    .build()
-                    .convert(&html)
-                    .unwrap();
+            let html = subpage.content().await?;
+            let md = HtmlToMarkdown::builder()
+                .skip_tags(vec!["script", "style", "header", "footer"])
+                .add_handler(vec!["a"], |e: Element| Some(e.content.to_string()))
+                .build()
+                .convert(&html)
+                .unwrap();
 
-                let formatted_name = href
-                    .trim_start_matches("/donate-blood/blood-donation-process/")
-                    .trim_start_matches("/donate-blood/how-to-donate/")
-                    .trim_start_matches("/donate-blood/")
-                    .trim_end_matches(".html")
-                    .replace('/', "_")
-                    .replace('-', "_");
+            let formatted_name = href
+                .trim_start_matches("/donate-blood/blood-donation-process/")
+                .trim_start_matches("/donate-blood/how-to-donate/")
+                .trim_start_matches("/donate-blood/")
+                .trim_end_matches(".html")
+                .replace('/', "_")
+                .replace('-', "_");
 
-                let filename = format!("{}.md", formatted_name);
-                let path = output_dir.join(filename);
-                tokio::fs::write(path, md).await?;
+            let filename = format!("{}.md", formatted_name);
+            let path = output_dir.join(filename);
+            tokio::fs::write(path, md).await?;
 
-                subpage.close().await?;
-            }
+            subpage.close().await?;
         }
     }
 
