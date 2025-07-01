e2758104133ced5a80c6abc4bae9668cc2fe6ea6
akagiyuu <khanghuynh0245@gmail.com>
2025-07-01 15:48:08 +0700

    feat: use redis for session

--- backend/src/middleware/session.rs
+++ backend/src/middleware/session.rs
@@ -1,10 +1,18 @@
 use tower_sessions::{
-    Expiry, MemoryStore, SessionManagerLayer,
+    Expiry, SessionManagerLayer,
     cookie::{SameSite, time::Duration},
 };
+use tower_sessions_redis_store::{
+    RedisStore,
+    fred::prelude::{ClientLike, Config, Pool},
+};
 
-pub fn session() -> SessionManagerLayer<MemoryStore> {
-    let session_store = MemoryStore::default();
+pub async fn session() -> SessionManagerLayer<RedisStore<Pool>> {
+    let pool = Pool::new(Config::default(), None, None, None, 6).unwrap();
+
+    pool.wait_for_connect().await.unwrap();
+
+    let session_store = RedisStore::new(pool);
 
     SessionManagerLayer::new(session_store)
         .with_secure(false)
