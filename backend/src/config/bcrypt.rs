b43b3ca6f78087e3818f5ccb1256fee9096e301e
akagiyuu <khanghuynh0245@gmail.com>
2025-07-02 19:13:33 +0700

    feat(config/bcrypt): accept string as salt

--- backend/src/config/bcrypt.rs
+++ backend/src/config/bcrypt.rs
@@ -1,7 +1,10 @@
 use serde::Deserialize;
+use serde_with::{Bytes, serde_as};
 
+#[serde_as]
 #[derive(Debug, Deserialize)]
 pub struct BcryptConfig {
     pub cost: u32,
+    #[serde_as(as = "Bytes")]
     pub salt: [u8; 16],
 }
