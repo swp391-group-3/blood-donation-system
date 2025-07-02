37e034bcf872f6055abd8933add6dbda480b1c7b
ChiBao <baotochi87@gmail.com>
2025-07-02 18:50:06 +0700

    feat(analysis): add custom hook to get all blood requests

---
+++ frontend/src/hooks/use-get-all-request.ts
@@ -0,0 +1,15 @@
+import { deserialize, fetchWrapper } from "@/lib/api"
+import { BloodRequest } from "@/lib/api/dto/blood-request"
+import { useQuery } from "@tanstack/react-query"
+
+
+export const useGetAllRequest= ()=>{
+    return useQuery({
+        queryFn: async ()=>{
+            const response = await fetchWrapper("/blood-request")
+
+            return await deserialize<BloodRequest[]>(response)
+        },
+        queryKey: ["blood-request"]
+    })
+}
