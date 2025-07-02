2f69d78143f65d48153aaa5917748ba4dcc3eb8c
ChiBao <baotochi87@gmail.com>
2025-07-02 18:49:57 +0700

    feat(analysis): add custom hook to get all accounts

---
+++ frontend/src/hooks/use-get-all-account.ts
@@ -0,0 +1,15 @@
+import { deserialize, fetchWrapper } from "@/lib/api"
+import { Account } from "@/lib/api/dto/account";
+import { useQuery } from "@tanstack/react-query"
+
+
+export const useGetAllAccounts = ()=>{
+    return useQuery({
+        queryFn: async () => {
+            const response = await fetchWrapper("/account");
+
+            return await deserialize<Account[]>(response);
+        },
+        queryKey: ['account']
+    })
+}
