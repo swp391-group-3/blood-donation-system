1342b8a5a15c29974b9707bf3bb56694ba386a18
akagiyuu <khanghuynh0245@gmail.com>
2025-07-03 15:52:53 +0700

    fix(config): remove default frontend_url

--- backend/src/config/cors.rs
+++ backend/src/config/cors.rs
@@ -4,24 +4,10 @@ fn default_frontend_dev_origin() -> String {
     "http://localhost:3000".to_string()
 }
 
-fn default_frontend_origin() -> String {
-    "http://frontend:3000".to_string()
-}
-
 #[derive(Debug, Deserialize)]
 pub struct CorsConfig {
     #[serde(default = "default_frontend_dev_origin")]
     pub frontend_dev_origin: String,
 
-    #[serde(default = "default_frontend_origin")]
     pub frontend_origin: String,
-}
-
-impl Default for CorsConfig {
-    fn default() -> Self {
-        Self {
-            frontend_dev_origin: default_frontend_dev_origin(),
-            frontend_origin: default_frontend_origin(),
-        }
-    }
 }
