1cacc696190166e49c8c06a404f0ee475a16eb79
namdang-fdp <namdangcoder@gmail.com>
2025-07-09 22:13:26 +0700

    feat: add logout

--- frontend/src/hooks/use-logout.ts
+++ frontend/src/hooks/use-logout.ts
@@ -1,16 +1,19 @@
 import { fetchWrapper, throwIfError } from '@/lib/api';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { toast } from 'sonner';
 
 export const useLogout = () => {
+    const queryClient = useQueryClient();
+    console.log('logout');
     const mutation = useMutation({
         mutationFn: async () => {
             const response = await fetchWrapper('/auth/logout');
             await throwIfError(response);
         },
         onError: (error) => toast.error(error.message),
-        onSuccess: () => {
+        onSuccess: async () => {
             toast.message('Logout successfully');
+            await queryClient.invalidateQueries({ queryKey: ['auth', 'me'] });
         },
     });
     return { mutation };
