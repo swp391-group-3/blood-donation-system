198555e7dcb570f4d49dc7a0073f745ff117e5ae
ChiBao <baotochi87@gmail.com>
2025-07-11 13:38:27 +0700

    feat(admin-blog): add blog actions for table and blogPage

--- frontend/src/app/admin/blog/column.tsx
+++ frontend/src/app/admin/blog/column.tsx
@@ -1,6 +1,12 @@
+"use client"
+import { FormEdit } from "@/components/edit-profile";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { Blog } from "@/lib/api/dto/blog";
 import { createColumnHelper } from "@tanstack/react-table"
-import { headers } from "next/headers";
+import { Edit3, MoreHorizontal, Trash2 } from "lucide-react";
+import { useState } from "react";
 
 
 const columnHelper = createColumnHelper<Blog>();
@@ -46,9 +52,11 @@ export const columns = [
         header: "Owner",
         cell: ({ row }) => {
             const blog = row.original;
-            <div>
-                <div className="text-sm text-gray-500">{blog.owner}</div>
-            </div>
+            return (
+                <div>
+                    <div className="text-sm text-gray-500">{blog.owner}</div>
+                </div>
+            )
         }
     }),
     columnHelper.display({
@@ -58,9 +66,75 @@ export const columns = [
             const blog = row.original;
             return (
                 <div>
-                    Actions
+                    <BlogActionsCell blog={blog} />
                 </div>
             )
         }
     })
 ]
+
+function BlogActionsCell({ blog }: { blog: Blog }) {
+    const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+    // const { mutation, form } = useUpdateDataTable(blog.id, blog, {
+    //     onSuccess() {
+    //         setIsEditModalOpen(false);
+    //     },
+    // });
+    // const deleteMutation = useDeleteAccount();
+    // const handleDeleteAccount = (id: string) => {
+    //     deleteMutation.mutate(id);
+    // };
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="h-8 w-8 p-0">
+                    <MoreHorizontal className="h-4 w-4" />
+                </Button>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent
+                align="start"
+                side="bottom"
+                sideOffset={8}
+                alignOffset={4}
+            >
+                {/* EDIT */}
+                <DropdownMenuItem
+                    onSelect={() => {
+                        setIsEditModalOpen(true);
+                    }}
+                >
+                    <Edit3 className="mr-2 h-4 w-4" />
+                    Edit Blog
+                </DropdownMenuItem>
+
+                <DropdownMenuSeparator />
+
+                {/* DELETE */}
+                <DropdownMenuItem
+                    // onSelect={() => {
+                    //     handleDeleteAccount(account.id);
+                    // }}
+                    className="text-red-600"
+                >
+                    <Trash2 className="mr-2 h-4 w-4" />
+                    Delete Blog
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+
+            <Dialog open={isEditModalOpen} onOpenChange={setIsEditModalOpen}>
+                <DialogContent className="max-h-[90vh] overflow-hidden flex flex-col">
+                    <DialogHeader>
+                        <DialogTitle>Edit Blog</DialogTitle>
+                    </DialogHeader>
+                    <div className="overflow-y-auto flex-1 pr-2">
+                        {/* <FormEdit 
+                        form={form} 
+                        mutation={mutation}
+                         /> */}
+                    </div>
+                </DialogContent>
+            </Dialog>
+        </DropdownMenu>
+    )
+}
--- frontend/src/app/admin/blog/page.tsx
+++ frontend/src/app/admin/blog/page.tsx
@@ -1,9 +1,208 @@
-import React from 'react'
+"use client"
+import { useBlogList } from '@/hooks/use-blog-list'
+import { flexRender, getCoreRowModel, getPaginationRowModel, useReactTable } from '@tanstack/react-table'
+import React, { useMemo, useState } from 'react'
+import { columns } from './column';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Pagination, PaginationContent, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from '@/components/ui/pagination';
+import { Search } from 'lucide-react';
+import { Input } from '@/components/ui/input';
+import { Blog } from '@/lib/api/dto/blog';
 
-function Blog() {
+
+
+function BlogPage() {
+    const [searchTerm, setSearchTerm] = useState('');
+    const { data: blogs = [] } = useBlogList();
+
+
+    const filterBlogs: Blog[] = useMemo(() => {
+        return blogs.filter((blog) => {
+            console.log(searchTerm);
+
+            const searchTermLowerCase = searchTerm.toLowerCase()
+            const match = blog.content.toLowerCase().includes(searchTermLowerCase)
+                || blog.title.toLowerCase().includes(searchTermLowerCase)
+            return match;
+        })
+    }, [blogs, searchTerm])
+    const table = useReactTable({
+        data: filterBlogs,
+        columns: columns,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        initialState: {
+            pagination: {
+                pageIndex: 0,
+                pageSize: 5,
+            }
+        }
+    })
+
     return (
-        <div>Blog</div>
+        <div>
+            <div className='max-w-6xl mx-auto'>
+                {/* search */}
+                <Card className="mb-6 border-none">
+                    <CardContent className="pt-6 px-0">
+                        <div className="flex flex-col sm:flex-row gap-4">
+                            <div className="relative flex-1">
+                                <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-4 w-4" />
+                                <Input
+                                    placeholder="Search blog name"
+                                    value={searchTerm}
+                                    onChange={(e) =>
+                                        setSearchTerm(e.target.value)
+                                    }
+                                    className="pl-10"
+                                />
+                            </div>
+
+                        </div>
+                    </CardContent>
+                </Card>
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Blog</CardTitle>
+                        <CardDescription>
+                            Manage Blogs
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <Table>
+                            <TableHeader>
+                                {table.getHeaderGroups().map((headerGroup) => (
+                                    <TableRow key={headerGroup.id}>
+                                        {headerGroup.headers.map((header) => {
+                                            return (
+                                                <TableHead key={header.id}>
+                                                    {header.isPlaceholder
+                                                        ? null
+                                                        : flexRender(
+                                                            header.column
+                                                                .columnDef
+                                                                .header,
+                                                            header.getContext(),
+                                                        )}
+                                                </TableHead>
+                                            );
+                                        })}
+                                    </TableRow>
+                                ))}
+                            </TableHeader>
+                            <TableBody>
+                                {table.getRowModel().rows.map((row) => {
+                                    return (
+                                        <TableRow
+                                            key={row.id}
+                                            style={{
+                                                backgroundColor:
+                                                    row.getIsSelected()
+                                                        ? '#e3f2fd'
+                                                        : 'white',
+                                            }}
+                                        >
+                                            {row
+                                                .getVisibleCells()
+                                                .map((cell) => {
+                                                    return (
+                                                        <TableCell
+                                                            key={cell.id}
+                                                        >
+                                                            {flexRender(
+                                                                cell.column
+                                                                    .columnDef
+                                                                    .cell,
+                                                                cell.getContext(),
+                                                            )}
+                                                        </TableCell>
+                                                    );
+                                                })}
+                                        </TableRow>
+                                    );
+                                })}
+                            </TableBody>
+                        </Table>
+                    </CardContent>
+                    <CardFooter>
+                        <Pagination>
+                            <PaginationContent>
+                                <PaginationItem>
+                                    <PaginationPrevious
+                                        href="#"
+                                        onClick={(e) => {
+                                            e.preventDefault();
+                                            table.previousPage();
+                                        }}
+                                        aria-disabled={
+                                            !table.getCanPreviousPage()
+                                        }
+                                        tabIndex={
+                                            !table.getCanPreviousPage()
+                                                ? -1
+                                                : undefined
+                                        }
+                                        className={
+                                            !table.getCanPreviousPage()
+                                                ? 'pointer-events-none opacity-50'
+                                                : undefined
+                                        }
+                                    />
+                                </PaginationItem>
+                                {/* NUMBERS */}
+                                {table.getPageOptions().map((pageIndex) => {
+                                    const isCurrent =
+                                        pageIndex ===
+                                        table.getState().pagination.pageIndex;
+                                    return (
+                                        <PaginationItem key={pageIndex}>
+                                            <PaginationLink
+                                                href="#"
+                                                onClick={(e) => {
+                                                    e.preventDefault();
+                                                    table.setPageIndex(
+                                                        pageIndex,
+                                                    );
+                                                }}
+                                                aria-current={
+                                                    isCurrent
+                                                        ? 'page'
+                                                        : undefined
+                                                }
+                                            >
+                                                {pageIndex + 1}
+                                            </PaginationLink>
+                                        </PaginationItem>
+                                    );
+                                })}
+                                <PaginationItem>
+                                    <PaginationNext
+                                        href="#"
+                                        onClick={(e) => {
+                                            e.preventDefault();
+                                            table.nextPage();
+                                        }}
+                                        aria-disabled={!table.getCanNextPage()}
+                                        tabIndex={
+                                            !table.getCanNextPage()
+                                                ? -1
+                                                : undefined
+                                        }
+                                        className={
+                                            !table.getCanNextPage()
+                                                ? 'pointer-events-none opacity-50'
+                                                : undefined
+                                        }
+                                    />
+                                </PaginationItem>
+                            </PaginationContent>
+                        </Pagination>
+                    </CardFooter>
+                </Card>
+            </div>
+        </div>
     )
 }
 
-export default Blog
+export default BlogPage
