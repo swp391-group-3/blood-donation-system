7a56446bf2e5cdb317cf543dea742004f4a8d870
akagiyuu <khanghuynh0245@gmail.com>
2025-07-11 11:47:14 +0700

    feat(blood-request): add update blood request hook

---
+++ frontend/src/hooks/use-update-blood-request-form.ts
@@ -0,0 +1,51 @@
+'use client';
+
+import { priorities } from './../lib/api/dto/blood-request';
+import { z } from 'zod';
+import { fetchWrapper, throwIfError } from '@/lib/api';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+
+export const schema = z.object({
+    title: z.string().min(1, 'The title cannot be empty'),
+    priority: z.enum(priorities, {
+        errorMap: () => ({ message: 'Invalid Priority' }),
+    }),
+    max_people: z.number().min(1, 'At least one people'),
+});
+
+export const useUpdateBloodRequestForm = (id: string) => {
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation({
+        mutationFn: async (values: z.infer<typeof schema>) => {
+            const response = await fetchWrapper(`/blood-request/${id}`, {
+                method: 'PUT',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(values),
+            });
+
+            await throwIfError(response);
+        },
+        onError: (error) => toast.error(error.message),
+        onSuccess: () => {
+            toast.success('Update blood request successfully');
+            queryClient.invalidateQueries({ queryKey: ['blood-requests'] });
+        },
+    });
+
+    const form = useForm<z.infer<typeof schema>>({
+        resolver: zodResolver(schema),
+        defaultValues: {},
+    });
+
+    return {
+        mutation,
+        form,
+    };
+};
