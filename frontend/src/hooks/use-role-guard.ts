5fa1e3c2ccdb27289f6a0f8e0d67ccf573a9658b
akagiyuu <khanghuynh0245@gmail.com>
2025-07-14 15:48:28 +0700

    fix: use array for role guard

--- frontend/src/hooks/use-role-guard.ts
+++ frontend/src/hooks/use-role-guard.ts
@@ -5,13 +5,13 @@ import { Account, Role } from '@/lib/api/dto/account';
 import { useRouter } from 'next/navigation';
 import { toast } from 'sonner';
 
-export const useRoleGuard = async (requiredRoles: Set<Role>) => {
+export const useRoleGuard = async (requiredRoles: Role[]) => {
     const router = useRouter();
 
     const response = await fetchWrapper('/auth/me');
     const account = await deserialize<Account>(response);
 
-    if (requiredRoles.has(account.role)) {
+    if (requiredRoles.indexOf(account.role) !== -1) {
         return;
     }
 
