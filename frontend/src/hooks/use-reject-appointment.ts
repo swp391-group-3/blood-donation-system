096425710a7cf16490098eeaac7d1634748fdd31
namdang-fdp <namdangcoder@gmail.com>
2025-07-17 18:23:36 +0700

    feat: change useRejectAppointment to receive reason

--- frontend/src/hooks/use-reject-appointment.ts
+++ frontend/src/hooks/use-reject-appointment.ts
@@ -6,11 +6,14 @@ import { toast } from 'sonner';
 
 export const useRejectAppointment = (id: string) => {
     return useMutation({
-        mutationFn: async () => {
+        mutationFn: async (reason: string) => {
             const response = await fetchWrapper(`/appointment/${id}/reject`, {
                 method: 'PATCH',
+                body: JSON.stringify({ reason }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
             });
-
             await throwIfError(response);
         },
         onError: (error) => toast.error(error.message),
