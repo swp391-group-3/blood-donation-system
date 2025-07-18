25c2c33f730feb8070fc66672ad58a6304ad3070
akagiyuu <khanghuynh0245@gmail.com>
2025-07-18 14:33:11 +0700

    feat(util): add default pagination config

--- backend/src/util/pagination.rs
+++ backend/src/util/pagination.rs
@@ -1,7 +1,28 @@
 use serde::Deserialize;
+use utoipa::ToSchema;
 
-#[derive(Deserialize)]
+const fn default_page_size() -> usize {
+    10
+}
+
+const fn default_page_index() -> usize {
+    0
+}
+
+#[derive(Deserialize, ToSchema)]
 pub struct Pagination {
-    page_index: usize,
+    #[serde(default = "default_page_size")]
     page_size: usize,
+
+    #[serde(default = "default_page_index")]
+    page_index: usize,
+}
+
+impl Default for Pagination {
+    fn default() -> Self {
+        Self {
+            page_size: default_page_size(),
+            page_index: default_page_index(),
+        }
+    }
 }
