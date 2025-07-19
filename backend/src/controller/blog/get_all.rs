8663623786d28b3169aaf1de791e76ba63da1d44
Lunarcd <nhatanhth0212@gmail.com>
2025-07-20 01:42:43 +0700

    feat(blog): add pagination and query for get all api

--- backend/src/controller/blog/get_all.rs
+++ backend/src/controller/blog/get_all.rs
@@ -2,29 +2,66 @@ use axum::{
     Json,
     extract::{Query, State},
 };
-use database::queries::{self, blog::Blog};
+use database::{
+    client::Params,
+    queries::{
+        self,
+        blog::{Blog, GetAllParams},
+    },
+};
 use serde::Deserialize;
 use std::sync::Arc;
-use utoipa::ToSchema;
+use utoipa::{IntoParams, ToSchema};
 
 use crate::{
     error::{Error, Result},
     state::ApiState,
+    util::pagination,
 };
 
 #[derive(Deserialize, ToSchema)]
-#[schema(as = blod::create::Request)]
+pub enum Mode {
+    MostRecent,
+    OldestFirst,
+    TitleAZ,
+}
+
+fn default_mode() -> Mode {
+    Mode::MostRecent
+}
+
+impl Mode {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Mode::MostRecent => "Most Recent",
+            Mode::OldestFirst => "Oldest First",
+            Mode::TitleAZ => "Title A-Z",
+        }
+    }
+}
+
+#[derive(Deserialize, IntoParams)]
+#[into_params(parameter_in = Query)]
 pub struct Request {
     pub query: Option<String>,
+
+    pub tag: Option<String>,
+
+    #[serde(default = "default_mode")]
+    pub mode: Mode,
+
+    #[serde(default = "pagination::default_page_size")]
+    pub page_size: usize,
+
+    #[serde(default = "pagination::default_page_index")]
+    pub page_index: usize,
 }
 
 #[utoipa::path(
     get,
     tag = "Blog",
     path = "/blog",
-    params(
-        ("query" = Option<String>, Query, description = "Filter by blog name"),
-    ),
+    params(Request),
     responses(
         (status = 200, description = "Get Blog Successfully", body = Blog)
     )
@@ -36,7 +73,16 @@ pub async fn get_all(
     let database = state.database().await?;
 
     match queries::blog::get_all()
-        .bind(&database, &request.query)
+        .params(
+            &database,
+            &GetAllParams {
+                query: request.query,
+                tag: request.tag,
+                mode: request.mode.as_str(),
+                page_size: request.page_size as i32,
+                page_index: request.page_index as i32,
+            },
+        )
         .all()
         .await
     {
