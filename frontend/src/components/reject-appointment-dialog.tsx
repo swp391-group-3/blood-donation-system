9d3348e0a49e796b60355f8c1ec7cc5b14a13404
namdang-fdp <namdangcoder@gmail.com>
2025-07-22 13:19:03 +0700

    feat: add checkbox ban component

--- frontend/src/components/reject-appointment-dialog.tsx
+++ frontend/src/components/reject-appointment-dialog.tsx
@@ -8,6 +8,8 @@ import {
     DialogFooter,
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Checkbox } from '@/components/ui/checkbox';
 import { Button } from '@/components/ui/button';
 import { useState } from 'react';
 import { useRejectAppointment } from '@/hooks/use-reject-appointment';
@@ -23,6 +25,7 @@ export const RejectAppointmentDialog = ({
     onOpenChange,
     appointmentId,
 }: RejectAppointmentDialogProps) => {
+    const [isBanned, setIsBanned] = useState(false);
     const [reason, setReason] = useState('');
     const reject = useRejectAppointment(appointmentId);
 
@@ -41,6 +44,19 @@ export const RejectAppointmentDialog = ({
                         onChange={(e) => setReason(e.target.value)}
                     />
                 </div>
+                <Label className="hover:bg-accent/50 mt-4 flex items-start gap-3 rounded-lg border p-3 has-[[aria-checked=true]]:border-blue-600 has-[[aria-checked=true]]:bg-blue-50 dark:has-[[aria-checked=true]]:border-blue-900 dark:has-[[aria-checked=true]]:bg-blue-950">
+                    <Checkbox
+                        id="is-banned"
+                        checked={isBanned}
+                        onCheckedChange={(checked) => setIsBanned(!checked)}
+                        className="data-[state=checked]:border-rose-600 data-[state=checked]:bg-rose-600 data-[state=checked]:text-white dark:data-[state=checked]:border-rose-700 dark:data-[state=checked]:bg-rose-700"
+                    />
+                    <div className="grid gap-1.5 font-normal">
+                        <p className="text-sm leading-none font-medium">
+                            Ban permanently this user
+                        </p>
+                    </div>
+                </Label>
                 <DialogFooter>
                     <Button
                         variant="outline"
@@ -50,7 +66,12 @@ export const RejectAppointmentDialog = ({
                     </Button>
                     <Button
                         disabled={!reason || reject.isPending}
-                        onClick={() => reject.mutate(reason)}
+                        onClick={() =>
+                            reject.mutate({
+                                reason,
+                                isBanned: isBanned,
+                            })
+                        }
                     >
                         {reject.isPending ? 'Rejecting...' : 'Confirm Reject'}
                     </Button>
