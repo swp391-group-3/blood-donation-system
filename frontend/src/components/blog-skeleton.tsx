e58570344bf2e2863e35c4a0024f4078b7568147
ChiBao <baotochi87@gmail.com>
2025-07-23 16:51:53 +0700

    style(skeleton): change style of blog card skeleton

--- frontend/src/components/blog-skeleton.tsx
+++ frontend/src/components/blog-skeleton.tsx
@@ -3,9 +3,9 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export default function BlogCardSkeleton() {
     return (
-        <Card className="flex flex-col h-full border-zinc-50 rounded-lg shadow-xs shadow-black/20g transition-all duration-200">
+        <Card className="flex flex-col h-full border-zinc-100 rounded-lg shadow-xs shadow-black/20g transition-all duration-200">
             <CardHeader className="flex-1 pt-1 pb-2 px-5">
-                <div className="flex items-center gap-3 mb-3">
+                <div className="flex items-center gap-3 mb-3 bg-slate-100">
                     <Skeleton className="h-8 w-8 rounded-full" />
                     <div className="flex flex-col gap-1">
                         <Skeleton className="h-4 w-24 rounded" />
@@ -17,7 +17,7 @@ export default function BlogCardSkeleton() {
                     <Skeleton className="h-4 w-3/4 rounded" />
                 </CardTitle>
                 <CardContent className="p-0">
-                    <div className="space-y-1 mb-3 min-h-[56px]">
+                    <div className="space-y-1 mb-3 min-h-[56px] bg-slate-100">
                         <Skeleton className="h-3 w-full rounded" />
                         <Skeleton className="h-3 w-full rounded" />
                         <Skeleton className="h-3 w-5/6 rounded" />
@@ -25,7 +25,7 @@ export default function BlogCardSkeleton() {
                 </CardContent>
             </CardHeader>
             <div className="flex-1 flex flex-col justify-end">
-                <div className="flex flex-wrap gap-1.5 px-5 pb-4">
+                <div className="flex flex-wrap gap-1.5 px-5 pb-4 bg-slate-100">
                     <Skeleton className="h-5 w-12 rounded-full" />
                     <Skeleton className="h-5 w-14 rounded-full" />
                     <Skeleton className="h-5 w-10 rounded-full" />
