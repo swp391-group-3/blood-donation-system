73b669716d8c6366b71ef4a73736961cef0cc423
akagiyuu <khanghuynh0245@gmail.com>
2025-07-23 13:47:20 +0700

    feat(blog): add pagination to hook

--- frontend/src/hooks/use-blog-list.ts
+++ frontend/src/hooks/use-blog-list.ts
@@ -1,15 +1,24 @@
 'use client';
 
-import { deserialize, fetchWrapper } from '@/lib/api';
+import { buildParams, deserialize, fetchWrapper, Pagination } from '@/lib/api';
 import { Blog } from '@/lib/api/dto/blog';
 import { useQuery } from '@tanstack/react-query';
 
-export const useBlogList = () => {
+interface Filter {
+    query?: string;
+    page_size?: number;
+    page_index?: number;
+}
+
+export const useBlogList = (filter: Filter) => {
+    const params = buildParams(filter);
+
     return useQuery({
         queryFn: async () => {
-            const response = await fetchWrapper('/blog');
-            return await deserialize<Blog[]>(response);
+            const response = await fetchWrapper(`/blog?${params.toString()}`);
+
+            return await deserialize<Pagination<Blog>>(response);
         },
-        queryKey: ['blog'],
+        queryKey: ['blog', filter],
     });
 };
