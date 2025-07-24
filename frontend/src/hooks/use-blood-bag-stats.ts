493f055a49cd53409e0156db6db0a3509894c878
akagiyuu <khanghuynh0245@gmail.com>
2025-07-24 11:19:15 +0700

    feat: add blood bag stats api call

---
+++ frontend/src/hooks/use-blood-bag-stats.ts
@@ -0,0 +1,14 @@
+import { deserialize, fetchWrapper } from '@/lib/api';
+import { BloodBagStats } from '@/lib/api/dto/blood-bag';
+import { useQuery } from '@tanstack/react-query';
+
+export const useBloodBagStats = () => {
+    return useQuery({
+        queryFn: async () => {
+            const response = await fetchWrapper(`/blood-bag/stats`);
+
+            return await deserialize<BloodBagStats>(response);
+        },
+        queryKey: ['blood-request', 'stats'],
+    });
+};
